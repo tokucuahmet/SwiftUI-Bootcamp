fdd8c0bd67527539801dc34f1e32734e93a8ca92
Ahmet TOKUCU <37275317+tokucuahmet@users.noreply.github.com>
2025-03-04 23:02:49 +0300

    Ders 2: Dizayn Şekilleri (Shapes)
    SwiftUI’de Rectangle, Circle, Ellipse, Capsule, RoundedRectangle gibi temel şekillerin nasıl oluşturulacağı ve özelleştirileceği.

--- SwiftUISerisi/SwiftUISerisi/ContentView.swift
+++ SwiftUISerisi/SwiftUISerisi/ContentView.swift
@@ -11,46 +11,49 @@ struct ContentView: View {
     
     var body: some View {
         
-        HStack(spacing: 20) // Yatay hizalamaya yarar
-        {
-            Rectangle()
-                .fill(Color.red)
-                .frame(width: 100, height: 100)
-            Rectangle()
-                .fill(Color.blue)
-                .frame(width: 100, height: 100)
-            Rectangle()
-                .fill(Color.green)
-                .frame(width: 100, height: 100)
-        }
-        VStack(spacing: 50) // Dikey hizalamaya yarar
-        {
-            Rectangle()
-                .fill(Color.red)
-                .frame(width: 100, height: 100)
-            Rectangle()
-                .fill(Color.blue)
-                .frame(width: 100, height: 100)
-            Rectangle()
-                .fill(Color.green)
-                .frame(width: 100, height: 100)
-            Circle()
-                .fill(Color.yellow)
-                .frame(width: 100, height: 100)
-        }
-        .padding()
-        ZStack() // İç içe hizalamaya yarar
-        {
-            Rectangle()
-                .fill(Color.red)
-                .frame(width: 100, height: 100)
-            Rectangle()
-                .fill(Color.blue)
-                .frame(width: 100, height: 100)
-            Rectangle()
-                .fill(Color.green)
-                .frame(width: 100, height: 100)
-        }
+        Rectangle() // Dikdörtgen & Kare oluşturma
+                    .fill(Color.blue) // Renk ekleme
+                    .frame(width: 200, height: 200) // Boyut belirleme
+                    .cornerRadius(20) // Köşe yuvarlama
+                    .shadow(radius: 10) // Gölge ekleme
+                    .padding()
+        
+        Circle() // Daire oluşturma
+                    .fill(Color.green) // Dairenin rengi
+                    .frame(width: 150, height: 150) // Çap ayarlama
+                    .overlay( // Mevcut view'in üzerine başka bir view yerleştirir.
+                        Text("SwiftUI Öğreniyorum")
+                            .foregroundColor(.black) // Metnin rengi
+                            .font(.headline)
+                    )
+                    .padding()
+        
+        RoundedRectangle(cornerRadius: 25) // Yuvarlatılmış dikdörtgen oluşturma
+                    .fill(Color.orange)
+                    .frame(width: 200, height: 100)
+                    .overlay(
+                        Text("SwiftUI Shapes")
+                            .foregroundColor(.white)
+                            .font(.title)
+                    ) // Metin ekleme
+                    .shadow(radius: 5) // Gölge ekleme
+                    .padding()
+        
+        Ellipse() // Elips oluşturma
+                    .fill(Color.purple)
+                    .frame(width: 250, height: 150) // Elipsin boyutları
+                    .shadow(radius: 8)
+                    .overlay(
+                        Circle()
+                            .stroke(Color.white, lineWidth: 5)
+                            .padding(5)
+                            .position(x: 125, y: 75)
+                    )
+                    .overlay(
+                        Text("SwiftUI Shapes")
+                            .foregroundColor(.white)
+                            .background(Color.black)
+                    )
     }
 }
 
